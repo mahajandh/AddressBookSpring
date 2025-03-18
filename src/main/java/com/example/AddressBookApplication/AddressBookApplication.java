5127609545d7f54370a6bf38c3fa056fbb95a33f
mahajandh <dhruvmahajan73@gmail.com>
2025-03-18 15:41:48 +0530

    added logger patterns

--- src/main/java/com/example/AddressBookApplication/AddressBookApplication.java
+++ src/main/java/com/example/AddressBookApplication/AddressBookApplication.java
@@ -2,12 +2,18 @@ package com.example.AddressBookApplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import lombok.extern.slf4j.Slf4j;
 
+
+@Slf4j
 @SpringBootApplication
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Address-Book App Started in {} Environment",
+				context.getEnvironment().getProperty("spring.profiles.active"));
 	}
 
 }
